416e4ce6052df3c807eff23500d49e398a75f32a
Qingbiao Miao <einsn56@gmail.com>
2014-02-24 22:31:34 +0800

    Add comment

--- StackedAutoencoders/StackedAutoencoders/ca.h
+++ StackedAutoencoders/StackedAutoencoders/ca.h
@@ -14,6 +14,7 @@ public:
 	MatrixXd ZCAWhite(MatrixXd &x);
 };
 
+//Principal component analysis
 MatrixXd CA::PCA(MatrixXd &x,double ratio)
 {
 	if(ratio <= 0 || ratio > 1)
@@ -21,17 +22,23 @@ MatrixXd CA::PCA(MatrixXd &x,double ratio)
 		return MatrixXd::Zero(x.rows(),x.cols());
 	}
 	MatrixXd avg = x.rowwise().mean();
+	//Zero mean the data set
 	x = x - avg.replicate(1,x.cols());
+	//Calculate the covariance
 	MatrixXd sigma = x * x.transpose() * (1.0 / x.cols());
+	//Singular value decomposition
 	JacobiSVD<MatrixXd> svd(sigma, ComputeFullU | ComputeFullV);
 	MatrixXd U = svd.matrixU();
 	MatrixXd V = svd.matrixV();
 	MatrixXd S = svd.singularValues();
+	// Project the data set
 	MatrixXd xRot = U.transpose() * x;
+	//Calculate the covariance after projection
 	MatrixXd cov = xRot * xRot.transpose() * (1.0 / xRot.cols());
 	int k = 0;
 	double p = 0;
 	double curVar = 0;
+	//summation of singular values
 	double sumVar = S.array().sum();
 	for(k = 0;k < S.rows() && p < ratio;k++)
 	{
@@ -44,16 +51,21 @@ MatrixXd CA::PCA(MatrixXd &x,double ratio)
 	return xHat;
 }
 
+//Calculate the PCA White value of input
 MatrixXd CA::PCAWhite(MatrixXd &x)
 {
 	
 	MatrixXd avg = x.rowwise().mean();
+	//Zero mean the data set
 	x = x - avg.replicate(1,x.cols());
+	//Calculate the covariance
 	MatrixXd sigma = x * x.transpose() * (1.0 / x.cols());
+	//Singular value decomposition
 	JacobiSVD<MatrixXd> svd(sigma, ComputeFullU | ComputeFullV);
 	MatrixXd U = svd.matrixU();
 	MatrixXd V = svd.matrixV();
 	MatrixXd S = svd.singularValues();
+	//calculate the pca white value
 	double epsilon = 0.1;
 	MatrixXd term1 = S + MatrixXd::Ones(S.rows(),S.cols()) * epsilon;
 	MatrixXd term2 = reciprocal(sqrtMat(term1));
@@ -61,15 +73,20 @@ MatrixXd CA::PCAWhite(MatrixXd &x)
 	return xPCAWhite;
 }
 
+//Calculate the ZCA White value of input
 MatrixXd CA::ZCAWhite(MatrixXd &x)
 {
 	MatrixXd avg = x.rowwise().mean();
+	//Zero mean the data set
 	x = x - avg.replicate(1,x.cols());
+	//Calculate the covariance
 	MatrixXd sigma = x * x.transpose() * (1.0 / x.cols());
+	//Singular value decomposition
 	JacobiSVD<MatrixXd> svd(sigma, ComputeFullU | ComputeFullV);
 	MatrixXd U = svd.matrixU();
 	MatrixXd V = svd.matrixV();
 	MatrixXd S = svd.singularValues();
+	//calculate the pca white value
 	double epsilon = 0.1;
 	MatrixXd term1 = S + MatrixXd::Ones(S.rows(),S.cols()) * epsilon;
 	MatrixXd term2 = reciprocal(sqrt(term1));
